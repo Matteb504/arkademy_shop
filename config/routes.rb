33586bdbec83d7f1edb22a702e796f3d20558467
Mateusz <matteb504@gmail.com>
2021-06-11 11:18:10 +0200

    admin permissions added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :admin do 
     resources :products
   end
-  resources :products
+  resources :products, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
